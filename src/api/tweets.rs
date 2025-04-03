8ab0e269604344bd1cb7114b2f2b80af52a47703
Ahsen Kamal <itsahsenkamal@gmail.com>
2025-04-04 00:01:39 +0530

    add reverse chronological timeline endpoint
    Signed-off-by: Ahsen Kamal <itsahsenkamal@gmail.com>

--- src/api/tweets.rs
+++ src/api/tweets.rs
@@ -36,6 +36,14 @@ where
         self.send(self.request(Method::DELETE, self.url(format!("tweets/{id}"))?))
             .await
     }
+    
+    pub fn get_user_home_timeline(
+        &self,
+        user_id: impl IntoNumericId,
+    ) -> GetTimelineRequestBuilder<A, Vec<Tweet>, TweetsMeta> {
+        GetTimelineRequestBuilder::new(self, self.url(format!("users/{user_id}/timelines/reverse_chronological")).unwrap())
+    }
+
     pub fn get_user_tweets(
         &self,
         user_id: impl IntoNumericId,
